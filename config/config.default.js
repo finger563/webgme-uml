6cc1e2cfd3da0d45ddc67b99c614defc077e9b95
William Emfinger <waemfinger@gmail.com>
2017-06-03 19:38:13 -0500

    updated base seed to have plugin enabled and updated config for server.

--- config/config.default.js
+++ config/config.default.js
@@ -7,5 +7,22 @@ var config = require('./config.webgme'),
 config.server.port = 8082;
 // config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
 
+// Executors
+config.executor.enable = true;
+
+config.core.enableCustomConstraints = true;
+
+// Plugins
+config.plugin.allowServerExecution = true;
+//config.plugin.allowBrowserExecution = false;
+
+// Seeds
+config.seedProjects.enable = true;
+config.seedProjects.basePaths = ["./src/seeds/umlBase"]
+
+// Merging config
+config.storage.autoMerge.enable = true;
+
+
 validateConfig(config);
 module.exports = config;
